5192be92d64ef4b9fce6efb3b11712a335477110
Setora <n12445266@qut.edu.au>
2026-07-28 18:31:16 +1000

    Add variables practice

---
+++ variable.py
@@ -0,0 +1,39 @@
+name="setora"
+print(name.title())
+
+age=26
+print(type(age))
+
+height=1.60
+print(type(height))
+
+is_student=True
+print(type(is_student))
+
+#In Python, variables are used as labeled containers to store, 
+# reference, and manipulate data in a computer's memory. 
+# Instead of hardcoding raw values repeatedly, variables allow 
+# you to give data a meaningful name, making your code dynamic, 
+# reusable, and readable
+
+#CHallenge
+name = "Setora"
+age = 27
+country="Uzbekistan"
+university= "QUT"
+dream_job= "AI Automation"
+
+print("Name:", name)
+print("Age:", age)
+print("Country:", country)
+print("University:", university)
+print("Dream job:", dream_job)
+
+print(f"""
+Hello!
+My name is {name}.
+I am {age} years old.
+I am from {country}.
+I study at {university}.
+My dream job is {dream_job}.
+"""
